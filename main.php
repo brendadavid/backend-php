478c5311e4ac63f75c1becb3c3e0dd0deae6f472
BrendaDavid <42557879+brendadavid@users.noreply.github.com>
2020-01-20 20:39:31 -0300

    Update main.php

--- main.php
+++ main.php
@@ -4,6 +4,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Moovin\Job\Backend;
 
-$exemplo = new Backend\Exemplo;
+$transacaoCc = new CaixaEletronico("5678 678-1", "cc");
+$transacaoCc->depositar(1000);
+$transacaoCc->transferir(150, "1570 778-0");
+$transacaoCc->sacar(600);
 
-echo $exemplo->exemplo();
+$transacaoCp = new CaixaEletronico("1234 900-1", "cp");
+$transacaoCp->depositar(1000);
+$transacaoCp->transferir(300, "4321 455-0");
+$transacaoCp->sacar(1000);
